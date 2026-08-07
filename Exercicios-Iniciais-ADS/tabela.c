892e1ab1a0dca35cb76dc8df3203d61ebb3c4f1e
lm8519 <laisc8519@gmail.com>
2026-08-07 18:52:32 -0300

    Exercício "tabela.c"
    Exercício da faculdade para aprender \n e \t
    
    \n Quebras de linha em forma de tabela
    \t  para tabulação

---
+++ Exercicios-Iniciais-ADS/tabela.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main ()
+{
+    // Usando \n para quebras de linha e \t para tabulação
+    printf("Lista de Produtos: \n");
+    printf("Produto\t\tPreco\t\tQuantidade\n");
+    printf("---------------------------------------------------------\n");
+    printf("Cafe\t\tR$ 3.50\t\t10\n");
+    printf("Pao\t\tR$ 2.00\t\t20\n");
+    printf("Leite\t\tR$ 4.50\t\t15\n");
+    
+    // Mais uma linha para pular
+    printf("\nResumo da compra:\n");
+    printf("\t- Total de produtos: 3\n");
+    printf("\t- Valor total: R$ 10.00\n");
+
+    return 0;
+}
