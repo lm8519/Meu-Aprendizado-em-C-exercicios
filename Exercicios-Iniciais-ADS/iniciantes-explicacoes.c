db56ca19a17518ec7a84e77a2fd3d5ee2f8f22ae
lm8519 <laisc8519@gmail.com>
2026-08-26 21:31:58 -0300

    atualizações

--- Exercicios-Iniciais-ADS/iniciantes-explicacoes.c
+++ Exercicios-Iniciais-ADS/iniciantes-explicacoes.c
@@ -9,16 +9,19 @@ int main (void)
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
     int n = 10; //atribuindo valor para variável
-    float n2 = 6.79; //float é uma variável que possui casas decimais
+    float n2 = 6.79; //float é uma variável que possui casas decimais 
     char letra = 'a'; //Variável do tipo caracter (uma unica letra)
     char frase[10] = "Bom dia"; //Variável do tipo "frase" (uma quantidade maior de caracteres)
-
+    double n3 = 1.23456; //Variável que possui casas decimais, porem comporta mais casas decimais do que o float (15) - real com precisão dupla
 
     printf("Hello World\n");
 
     printf("Exibindo o número inteiro %d\n", n);
-    printf("Exibindo um número real %f\n");
-
+    printf("Exibindo um número real %f\n", n2);
+    printf("Exibindo o caracter %c\n", letra);
+    printf("%s\n", frase);
+    printf("Exibindo variável do tipo double %f\n", n3);
+    printf("Valores: %d %f %c %s %f\n", n, n2, letra, frase, n3);
 
 return 0;
 }
