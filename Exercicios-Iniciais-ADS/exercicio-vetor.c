bb80d8a5a85068a4572c6811d5969b9d509d9365
lm8519 <laisc8519@gmail.com>
2026-09-21 21:18:05 -0300

    Exercício vetores "corrigidos"

--- Exercicios-Iniciais-ADS/exercicio-vetor.c
+++ Exercicios-Iniciais-ADS/exercicio-vetor.c
@@ -2,10 +2,12 @@
 # include <stdlib.h>
 # include <locale.h>
 
+// Como eu fiz o exercício 
 int main ()
 {
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
+    /*
     int v[20];
     int i;
 
@@ -21,9 +23,28 @@ int main ()
     {
         printf("O %dº inteiro é: %d \n", i + 1, v[i]);
     }
-    
-    
-    
+    */
 
+// Como o professor fez (eu esqueci de dizer quais são pares e quais são ímpares)
+    int v[20];
+    int i, pares=0;
+
+    for(i=0;i<20;i++){
+        printf("Digite o %io valor: ",i+1);
+        scanf("%i", &v[i]);
+    }
+
+    for(i=0;i<20;i++){
+        printf("%i\t", v[i]);
+    }
+    printf("\nPares: ");
+    for(i=0;i<20;i++){
+        if(v[i]%2==0){
+            printf("%i\t", v[i]);
+            pares++;
+        }
+    }
+    printf("\nQuant. pares: %i\n", pares);
+    
 return 0;
 }
